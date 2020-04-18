bd35256f89120e380aa5075087ea6d888fc3d13b
Alok <kulkarni.alok@gmail.com>
2020-04-18 17:40:58 +0100

    add build, minification and terser plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,46 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+
+const dist = 'dist'
+const bundle = 'bundle'
+const name = 'ReactAKCssSpinners'
+const production = !process.env.ROLLUP_WATCH
+
 export default {
   input: 'src/index.js',
+  external: ['react'],
   output: [
     {
-      file: 'dist/bundle.cjs.js',
+      file: `${dist}/${bundle}.cjs.js`,
       format: 'cjs'
     },
     {
-      file: 'dist/bundle.umd.js',
+      name: `${name}`,
+      file: `${dist}/${bundle}.umd.js`,
+      globals: {
+        react: 'React'
+      },
       format: 'umd'
     },
     {
-      file: 'dist/bundle.esm.js',
+      file: `${dist}/${bundle}.esm.js`,
       format: 'esm'
+    },
+    {
+      name: `${name}`,
+      file: `${dist}/${bundle}.iife.js`,
+      globals: {
+        react: 'React'
+      },
+      format: 'iife'
     }
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    production && terser()
   ]
 }
